08b20dc2715bd8db7390b5660ec147d4bb612352
LenovoLaptop <mushmome1@gmail.ocm>
2020-10-01 16:02:47 -0400

    added

---
+++ src/BuddyInfo.java
@@ -0,0 +1,36 @@
+
+public class BuddyInfo {
+
+
+  private String name;
+  private String address;
+  private String phone;
+
+  public BuddyInfo(String name, String address, String phone) {
+    this.name = name;
+    this.address = address;
+    this.phone = phone;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+
+  public static void main(String[] args)
+  {
+  }
+
+
+
+
+
+}
